a7460c04bab0b1e019d07fe5be7f942c7ed5540e
Markus Seidler <mseidler79@gmail.com>
2021-03-27 19:02:30 +0800

    Initial Commit

---
+++ Shared/Enroute21App.swift
@@ -0,0 +1,17 @@
+//
+//  Enroute21App.swift
+//  Shared
+//
+//  Created by Markus Seidler on 27/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Enroute21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
